89260bfbc1c0e2effb677ca2036f27bb07f0dd0c
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-18 00:52:55 -0300

    Streams no Node.js - Criando middleware de JSON
    Nessa prática vamos conhecer o conceito de middlewares no node, separando a lógica de conversão dos dados em JSON para uma função separada, facilitando e deixando o código mais organizado.
    O middleware como um interceptador, que recebe a requisição que lida com essa requisição da melhor forma para a aplicação.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 const users = [];
 
@@ -6,22 +7,10 @@ const users = [];
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
-  const buffers = [];
-
-  for await (const chunk of request) {
-    buffers.push(chunk);
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    request.body = null;
-  }
+  await json(request, response);
 
   if (method === 'GET' && url === '/users') {
-    return response
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users));
+    return response.end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
