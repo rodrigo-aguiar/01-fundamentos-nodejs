0b4785e3aa8651c6401c4054d54e033432ee93f7
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-22 20:57:14 -0300

    Rotas da aplicação - Criando regex dos parâmetros
    Seguindo o desenvolvimento para acessar os parâmetros das rotas, vamos criar uma função que vai utilizar de uma RegEx para as futuras operações.

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import { randomUUID } from 'node:crypto'
+
 import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database();
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (request, response) => {
       const users = database.select('users');
 
@@ -15,7 +17,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (request, response) => {
       const { name, email } = request.body;
 
@@ -32,7 +34,7 @@ export const routes = [
   },
   {
     method: 'DELETE',
-    path: '/users/ID',
+    path: buildRoutePath('/users/:id'),
     handler: (request, response) => {}
   }
 ];
