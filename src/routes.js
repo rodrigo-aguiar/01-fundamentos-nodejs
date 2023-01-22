1821e3e9239dde008d7fd50f66567293b5af75a0
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-22 20:45:23 -0300

    Rotas da aplicação - Route e Query parameters
    Existem diversas formas de enviarmos informações para uma API. Nessa aula vamos aprender sobre elas e iniciar o desenvolvimento da estrutura para acessarmos essas informações.

--- src/routes.js
+++ src/routes.js
@@ -29,5 +29,10 @@ export const routes = [
 
     return response.writeHead(201).end();
     }
+  },
+  {
+    method: 'DELETE',
+    path: '/users/ID',
+    handler: (request, response) => {}
   }
 ];
