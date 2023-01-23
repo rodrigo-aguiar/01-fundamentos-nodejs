9fc8f17620ae1b7cb005b481af80b8e001519007
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-22 21:24:28 -0300

    Rotas da aplicação - Remoção de registros
    Agora que já concluímos a manipulação da URL por meio das RegEx, vamos obter o id de um usuário e criar a rota para remover ele.

--- src/server.js
+++ src/server.js
@@ -15,6 +15,8 @@ const server = http.createServer(async (request, response) => {
   if (route) {
     const routeParams = request.url.match(route.path);
 
+    request.params = { ...routeParams.groups };
+
     return route.handler(request, response);
   }
 
